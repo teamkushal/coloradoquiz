a3b4d60ece708bfab25f362e8a556a7507b4c59a
Kushal Hada <kus@tilde.team>
2020-12-31 10:34:05 -0700

    add loading service and component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingService } from './loading.service';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  constructor(
+    private loadingService: LoadingService,
+  ) { }
+
   ngOnInit(): void {
-    console.log(`Hello, world!`);
+    this.loadingService.turnLoadingOn();
   }
   title = 'coloradoquiz';
   }
