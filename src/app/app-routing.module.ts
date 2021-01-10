30d66cf0f79ef750626b3600b2dc5687ef871dc3
Kushal Hada <kus@tilde.team>
2021-01-10 12:32:48 -0700

    add a new quiz service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { QuizComponent } from './quiz/quiz.component';
 
 const routes: Routes = [
+  {
+    'path': 'quiz',
+    'component': QuizComponent
+  },
   {
     'path': 'home',
     'component': HomeComponent
