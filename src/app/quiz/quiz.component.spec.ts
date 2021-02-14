47094a459e936148396f6d493f2632e6947505cd
Kushal Hada <kus@tilde.team>
2021-02-13 18:51:35 -0700

    add activated route
    https://stackoverflow.com/a/51941301/227646

--- src/app/quiz/quiz.component.spec.ts
+++ src/app/quiz/quiz.component.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
 import { MaterialModule } from '../material/material.module';
 
 import { QuizComponent } from './quiz.component';
@@ -12,6 +13,12 @@ describe('QuizComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [ QuizComponent ],
       imports: [HttpClientTestingModule, MaterialModule],
+      providers: [
+        {
+          provide: ActivatedRoute, useValue: {
+            snapshot: { params: { title: "history" } }
+          }
+        }]
     })
     .compileComponents();
   });
