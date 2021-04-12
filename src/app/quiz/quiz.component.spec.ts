2084f04e31dd72ae9cb1d84594315e238761e5ca
Kushal Hada <kus@tilde.team>
2021-04-12 17:10:18 -0600

    update angular

--- src/app/quiz/quiz.component.spec.ts
+++ src/app/quiz/quiz.component.spec.ts
@@ -1,6 +1,7 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { MaterialModule } from '../material/material.module';
 
 import { QuizComponent } from './quiz.component';
@@ -12,7 +13,12 @@ describe('QuizComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ QuizComponent ],
-      imports: [HttpClientTestingModule, MaterialModule],
+      imports: [
+        HttpClientTestingModule,
+        MaterialModule,
+        RouterTestingModule.withRoutes(
+          [{path: '', component: QuizComponent}, {path: 'simple', component: QuizComponent}]
+        )],
       providers: [
         {
           provide: ActivatedRoute, useValue: {
