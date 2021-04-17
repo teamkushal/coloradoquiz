7e41075205be6c3f7f65cc154a22198b2adf64c4
Kushal Hada <kus@tilde.team>
2021-04-16 18:37:34 -0600

    add a new component for menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { KeyIndustriesComponent } from './key-industries/key-industries.component';
 import { NewsComponent } from './news/news.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     HomeComponent,
     QuizComponent,
     KeyIndustriesComponent,
-    NewsComponent
+    NewsComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
