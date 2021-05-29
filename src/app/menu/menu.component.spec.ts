fe8de60d6547e1d8b9584dcf260bc1ab679efd5b
Kushal Hada <kus@tilde.team>
2021-05-29 10:16:23 -0600

    add import for unit test

--- src/app/menu/menu.component.spec.ts
+++ src/app/menu/menu.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MaterialModule } from '../material/material.module';
 
 import { MenuComponent } from './menu.component';
 
@@ -8,9 +10,9 @@ describe('MenuComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
-    })
-    .compileComponents();
+      declarations: [MenuComponent],
+      imports: [HttpClientTestingModule, MaterialModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
