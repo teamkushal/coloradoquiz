c5fa201c3f77384e33e3e954e160f77f8e001203
Kushal Hada <kus@tilde.team>
2021-05-29 18:27:57 -0600

    add timestamp

--- update.sh
+++ update.sh
@@ -1,24 +1,80 @@
 #!/bin/bash
 
-free -h;
+cd ~/src/angular/coloradoquiz/;
+date > README.md;
+git add .;
+git commit -m "add timestamp";
+
+echo "\`\`\`bash" >> README.md;
+git add .;
+git commit -m "begin add system status";
+echo "System Memory" >> README.md;
+free -h >> README.md;
+echo "System Storage" >> README.md;
+du -sh .>> README.md;
+git add .;
+git commit -m "add system status";
+echo "\`\`\`" >> README.md;
+git add .;
+git commit -m "end add system status";
+
+
+echo "\`\`\`bash" >> README.md;
+git add .;
+git commit -m "begin update node";
+[[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh;  # This loads NVM
 source ~/.nvm/nvm.sh;
 time nvm install --lts;
 time nvm use --lts;
 time npm install --global @angular/cli yarn;
-cd ~/src/angular/angulartwelve/;
+time yarn run ng --version >> README.md;
+time yarn >> README.md;
+echo "\`\`\`" >> README.md;
 git add .;
-git commit -m "pre update angular";
-time yarn run ng --version;
+git commit -m "end update node";
+
 export NODE_OPTIONS="--max-old-space-size=6144";
+
+git add .;
+git commit -m "begin prepare to update angular";
 time yarn run ng update @angular/core @angular/cli @angular/material @angular/localize;
 git add .;
-git commit -m "update angular";
-echo '' >> README.md;
-date >> README.md;
-echo '' >> README.md;
+git commit -m "end prepare to update angular";
+
+echo "\`\`\`bash" >> README.md;
 git add .;
-git commit -m "post update angular";
+git commit -m "begin prepare to build angular";
+time yarn run ng build --configuration production >> README.md;
+echo "\`\`\`" >> README.md;
+git add .;
+git commit -m "end prepare to build angular";
+
+echo "\`\`\`bash" >> README.md;
+git add .;
+git commit -m "begin prepare to unit test angular";
+time yarn run ng test >> README.md;
+echo "\`\`\`" >> README.md;
+git add .;
+git commit -m "end prepare to unit test angular";
+
+date >> README.md;
+git add .;
+git commit -m "add timestamp";
 git pull --rebase origin master --strategy-option=ours;
+git add .;
+git commit -m "merge from remote";
 git push origin master;
-time yarn run ng build --configuration production;
-time yarn run ng test;
+
+
+du -sh dist/angulartwelve/;
+time rsync -av /home/kushal/src/angular/angulartwelve/dist/angulartwelve/ /home/kushal/src/angular/angulrtwelvegithubio;
+du -sh /home/kushal/src/angular/angulrtwelvegithubio;
+cd /home/kushal/src/angular/angulrtwelvegithubio/;
+git add .;
+git commit -m "build and deploy";
+COMMIT_ID=$(git rev-parse --verify HEAD)
+curl https://sentry.io/api/hooks/release/builtin/5768670/b50116ba5ad9986947365aded6efb721e66310c9557954a43fe58fe0c27df2a4/ \
+  -X POST \
+  -H 'Content-Type: application/json' \
+  -d '{"version": "'"$COMMIT_ID"'"}'
+time git push origin master;
