910c155ce249e14490e4ecc82dcb073c1bd3d4b2
Kushal Hada <kus@tilde.team>
2021-05-31 06:23:04 -0600

    add new services

--- src/app/handle-un-recoverable-state.service.ts
+++ src/app/handle-un-recoverable-state.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { SwUpdate } from "@angular/service-worker";
 
-@Injectable({
-  providedIn: 'root'
-})
-export class HandleUnRecoverableStateService {
+@Injectable()
+export class HandleUnrecoverableStateService {
+  constructor(updates: SwUpdate) {
+    updates.unrecoverable.subscribe(event => {
+      this.notifyUser(
+        `An error occurred that we cannot recover from:\n${event.reason}\n\n` +
+        'Please reload the page.');
+    });
+  }
 
-  constructor() { }
+  notifyUser(message: string) {
+    console.log(message);
+  }
 }
