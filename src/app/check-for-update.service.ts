1081493ead485e16f2ba039e7733232900eb9522
Kushal Hada <kus@tilde.team>
2021-05-31 06:08:46 -0600

    update check for update service
    from https://angular.io/guide/service-worker-communications#checking-for-updates

--- src/app/check-for-update.service.ts
+++ src/app/check-for-update.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { ApplicationRef, Injectable } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { concat, interval } from 'rxjs';
+import { first } from 'rxjs/operators';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class CheckForUpdateService {
 
-  constructor() { }
+  constructor(appRef: ApplicationRef, updates: SwUpdate) {
+    // Allow the app to stabilize first, before starting polling for updates with `interval()`.
+    const appIsStable$ = appRef.isStable.pipe(first(isStable => isStable === true));
+    const everySixHours$ = interval(6 * 60 * 60 * 1000);
+    const everySixHoursOnceAppIsStable$ = concat(appIsStable$, everySixHours$);
+
+    everySixHoursOnceAppIsStable$.subscribe(() => updates.checkForUpdate());
+  }
 }
