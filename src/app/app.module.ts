2130a4f26f9bc82b40d6f89d911d7bfba05b4531
Kushal Hada <kus@tilde.team>
2022-08-24 12:56:12 -0400

    add timestamp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8049,6 +8049,9 @@ import { HttpRequestInterceptorService } from './http-request-interceptor.servic
       multi: true
     }
   ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
   bootstrap: [AppComponent]
 })
 export class AppModule { }
