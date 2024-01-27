b1905c783254103f27fbeab34c7f880042ed7088
Kushal Hada <kus@tilde.team>
2024-01-27 16:25:00 -0500

    remove sample

--- src/app/ipinforesponse.ts
+++ src/app/ipinforesponse.ts
@@ -10,30 +10,3 @@ export interface Ipinforesponse {
   timezone: string,
 
 }
-
-
-// {
-//   "ip": "35.146.244.54",
-//   "hostname": "035-146-244-054.res.spectrum.com",
-//   "city": "Lewisville",
-//   "region": "Texas",
-//   "country": "US",
-//   "loc": "33.0532,-96.9999",
-//   "org": "AS11427 Charter Communications Inc",
-//   "postal": "75057",
-//   "timezone": "America/Chicago",
-//   "readme": "https://ipinfo.io/missingauth"
-// }
-
-
-// {
-// "ip": "35.146.244.54",
-// "hostname": "035-146-244-054.res.spectrum.com",
-// "city": "Lewisville",
-// "region": "Texas",
-// "country": "US",
-// "loc": "33.0532,-96.9999",
-// "org": "AS11427 Charter Communications Inc",
-// "postal": "75057",
-// "timezone": "America/Chicago"
-// }
