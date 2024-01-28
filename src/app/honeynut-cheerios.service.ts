22b63aacea9e74f6dea11dd87b9fc7aff611806c
Kushal Hada <kus@tilde.team>
2024-01-28 15:12:02 -0500

    add honey nut cheerios
    $ time yarn run ng generate interface HoneynutCheeriosResponse
    yarn run v1.22.21
    $ ng generate interface HoneynutCheeriosResponse
    CREATE src/app/honeynut-cheerios-response.ts (46 bytes)
    Done in 0.39s.
    
    real    0m0.542s
    user    0m0.559s
    sys     0m0.128s

--- src/app/honeynut-cheerios.service.ts
+++ src/app/honeynut-cheerios.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { LoadingService } from './loading.service';
+import { Ipinforesponse } from './ipinforesponse';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HoneynutCheeriosService {
+  private myBehaviorSubject: BehaviorSubject<Ipinforesponse>;
+  public myObservable$: Observable<Ipinforesponse>;
+  private subscription: Subscription = new Subscription();
 
-  constructor() { }
+
+  constructor(
+    private http: HttpClient,
+    private loadingService: LoadingService
+  ) {
+    this.myBehaviorSubject = new BehaviorSubject<Ipinforesponse>(this.createDummy());
+    this.myObservable$ = this.myBehaviorSubject as Observable<Ipinforesponse>;
+  }
+
+  createDummy(): Ipinforesponse {
+    return {
+      ip: "0.0.0.0",
+      hostname: "example.com",
+      city: "example city",
+      region: "example region",
+      country: "example country",
+      loc: "0, 0",
+      org: "example inc",
+      postal: "00000",
+      timezone: "UTC",
+      lastupdated: new Date()
+    };
+  }
 }
